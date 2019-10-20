809490d374828ed410570ffd6670838dc0013ada
steveespinoza <steveespinoza@localhost>
2019-10-20 10:08:41 -0600

    Avance 2

--- src/prograproject/JSHumanResources.java
+++ src/prograproject/JSHumanResources.java
@@ -3,6 +3,9 @@ package prograproject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class JSHumanResources {
 
@@ -10,7 +13,7 @@ public class JSHumanResources {
 
     public static void main(String[] args) throws IOException {
         System.out.println("**********************************");
-        System.out.println("* Bienvenido a la Verduleria J&S *");
+        System.out.println("* Bienvenido a la Verduleria J&B *");
         System.out.println("**********************************");
         System.out.println("");
         login();
@@ -18,23 +21,42 @@ public class JSHumanResources {
     }
 
     static public void login() throws IOException {
-        Administrator admin = new Administrator("admin", "pass1");
+        File loginCredentials = new File("loginData.txt");
+        String filepath = "loginData.txt";
+
         System.out.println("Ingrese su ID");
         String user = br.readLine();
         System.out.println("Ingrese su contraseña ");
         String pass = br.readLine();
-        
-        if ((user.equals(admin.getUser())) && ( pass.equals(admin.getPassword()))) {
-            hiring();
-        } else {
-            System.out.println("El usuario/contraseña es incorrecto");
-        }
+        verifyLogin(user, pass, filepath);
 
     }
 
-    static public void hiring() {
-        System.out.println("Test");
-    
+    private static void verifyLogin(String user, String password, String filepath) throws FileNotFoundException, IOException {
+        Scanner sc = new Scanner(new File(filepath));
+        boolean found = false;
+        String tempUserName = "";
+        String tempPassword = "";
+        try {
+            while (sc.hasNext() && !found) {
+                sc.useDelimiter("[,\n]");
+                tempUserName = sc.next();
+                tempPassword = sc.next();
+
+                if (tempUserName.trim().equals(user.trim()) && tempPassword.trim().equals(password.trim())) {
+                    found = true;
+                    Employees admin = new Employees();
+                    admin.editFields();
+                    login();
+
+                } else {
+                    System.out.println("Credenciales incorrectos. Pruebe otra vez.");
+                    login();
+                }
+                sc.close();
+            }
+        } catch (Exception e) {
+        }
     }
 
     public void mainMenu() {
